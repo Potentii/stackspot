5ca810dc4e4229e0890a5e11f93d4546ca497a79
Guilherme Reginaldo Ruella <potentii@gmail.com>
2025-06-18 23:52:02 -0300

    Update

---
+++ libs/stackspot-ai-ks.test.mjs
@@ -0,0 +1,17 @@
+// import { describe, it } from "node:test";
+import Stackspot from "./stackspot.mjs";
+
+describe('Stackspot - AI - KS', () => {
+
+	it.skip('Should be able to create new KS', async t => {
+
+		// await Stackspot.instance.ai.ks.createKs('test-ks', 'KS name', 'KS description', 'CUSTOM');
+		//
+		// expect(Stackspot.instance).to.eq(Stackspot.instance);
+	});
+
+
+
+
+});
+
